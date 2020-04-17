98b69e9d084f2e1d47edc5fab5e286139a4eca27
Unnati Palan <unnati.palan@gmail.com>
2020-04-17 23:46:54 +0530

    Added the os.walk module

--- os_module.py
+++ os_module.py
@@ -27,9 +27,21 @@ fd = "abc.txt"
 file = open(fd, 'r')
 text = file.read()
 print(text)
-os.close(file)
+#os.close(file)
 #NOTE: Error is thrown for the non-existence of the file or the permission privileges.
 
+#changing directory
+os.chdir('/home/unnati/CodeSnippets')
+
+# returns a 3-tuple
+for dirpath, dirname, filename in os.walk('/home/unnati/CodeSnippets'):
+    print('Current path: ',dirpath)
+    print('Directories: ', dirname)
+    print('Files: ', filename)
+    print()
+#walk is a generator that yields three values as it is walking the directory tree 
+#for each directory that it traverses and produces the directory path, it prints the direct within that path and the files within that path. 
+#It is useful to keep track of all the directories
 
 
 
