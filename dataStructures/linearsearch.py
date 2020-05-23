cb69b2b645f4dc78cbb10a5a8392434c04635252
Titash RC <titash@live.in>
2020-05-23 12:05:32 +0200

    Search Algos

---
+++ dataStructures/linearsearch.py
@@ -0,0 +1,14 @@
+def linearSearch(array,element):
+    position = 0
+    flag = False
+
+    while position < len(array) and not flag: #Not flag allows us to exit the loop when we encounter our element
+        if (array[position] == element):
+            flag = True
+        else:
+            position = position + 1
+    return flag
+
+A = [10,15,16,89]
+
+print("Is Element present in the Array: ",linearSearch(A,2))
